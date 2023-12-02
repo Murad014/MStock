38ab531791ab89cba8f7162025b1c0b187212780
Murad014 <quliyev.murad@yahoo.com>
2023-12-03 00:21:04 +0400

    Initialize the project.

---
+++ mstockRestAPI/mstockRestAPI/src/main/java/com/mstockRestAPI/mstockRestAPI/MstockRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mstockRestAPI.mstockRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstockRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstockRestApiApplication.class, args);
+	}
+
+}
---
+++ mstockRestAPI/mstockRestAPI/src/test/java/com/mstockRestAPI/mstockRestAPI/MstockRestApiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mstockRestAPI.mstockRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MstockRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
