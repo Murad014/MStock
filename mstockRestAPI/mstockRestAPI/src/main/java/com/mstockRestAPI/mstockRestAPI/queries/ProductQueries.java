56a8558487f1be31afb506d3ac2cb1a7ff3cc0f3
Murad014 <quliyev.murad@yahoo.com>
2024-01-22 18:57:28 +0400

    Added find by Product Name.

---
+++ mstockRestAPI/mstockRestAPI/src/main/java/com/mstockRestAPI/mstockRestAPI/queries/ProductQueries.java
@@ -0,0 +1,18 @@
+package com.mstockRestAPI.mstockRestAPI.queries;
+
+public class ProductQueries {
+
+    public static final String FIND_BY_BARCODE_AND_IS_ACTIVE =
+            "SELECT p FROM Product p " +
+                    "JOIN p.productBarcodeList b " +
+                    "WHERE b.barcode = :barcode " +
+                    "AND " +
+                    "p.isActive = :isActive";
+
+    public static final String FIND_BY_PRODUCT_NAME_WITH_LIKE_AND_IS_ACTIVE =
+            "SELECT p FROM Product p " +
+                    "WHERE LOWER(p.productName) " +
+                    "LIKE LOWER(CONCAT('%', :productName, '%')) " +
+                    "AND " +
+                    "p.isActive = :isActive";
+}
--- mstockRestAPI/mstockRestAPI/src/main/java/com/mstockRestAPI/mstockRestAPI/repository/ProductRepository.java
+++ mstockRestAPI/mstockRestAPI/src/main/java/com/mstockRestAPI/mstockRestAPI/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.mstockRestAPI.mstockRestAPI.repository;
 
 import com.mstockRestAPI.mstockRestAPI.entity.Company;
 import com.mstockRestAPI.mstockRestAPI.entity.Product;
+import com.mstockRestAPI.mstockRestAPI.queries.ProductQueries;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +12,11 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByIsActive(Byte isActive);
 
-    @Query("SELECT p FROM Product p JOIN p.productBarcodeList b WHERE b.barcode = :barcode")
-    List<Product> findByBarcode(@Param("barcode") String barcode);
+    @Query(value = ProductQueries.FIND_BY_BARCODE_AND_IS_ACTIVE)
+    List<Product> findByBarcode(@Param("barcode") String barcode,
+                                @Param("isActive") byte isActive);
+
+    @Query(value = ProductQueries.FIND_BY_PRODUCT_NAME_WITH_LIKE_AND_IS_ACTIVE)
+    List<Product> findByProductName(@Param("productName") String productName,
+                                    @Param("isActive") byte isActive);
 }
